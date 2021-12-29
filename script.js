f4f5016ba35ff88f37a91892676ffb2f4bb0437c
Eda Toktogulova <edatoktogulova2003@gmail.com>
2021-12-29 14:22:03 +0600

    added var

---
+++ script.js
@@ -0,0 +1,29 @@
+var studentFirstName = "Bakyt";
+var studentLastName = "Utanov";
+var studentAge = 20;
+var studentOccupied = false;
+
+var student = {
+    firstName: "Bakyt",
+    lastName: "Utanov",
+    age: 20,
+    occupied: false,
+};
+
+// output
+// console.log(student.firstName + " " + student.lastName);
+
+var dog = {
+    name: "Kumaiyk",
+    age: 2,
+}
+// update
+dog.age =3;
+// console.log(dog.age);
+
+//add 
+dog.color = "gray";
+// console.log(dog.color);
+
+//delete
+delete dog.color;
